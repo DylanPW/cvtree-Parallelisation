e3b16a681a390c5bc5ff2dfec94348e8c1ae9620
DylanPW <d.prykewatanabe@gmail.com>
2018-10-16 13:28:42 +1000

    Init Commit after Parallelisation

---
+++ cmake-build-debug/CMakeFiles/cvtree.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cvtree.dir/parallel.cpp.obj"
+  "cvtree.pdb"
+  "cvtree.exe"
+  "cvtree.exe.manifest"
+  "libcvtree.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cvtree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
